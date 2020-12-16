0d4c9d321512eec750c864ca7eae1f4c6464d690
emphasize <swen_g@t-online.de>
2020-12-16 22:19:31 +0100

    Update sht3x.py
    c+p typo

--- src/sht3x.py
+++ src/sht3x.py
@@ -47,7 +47,7 @@ parser.add_argument(
     help='Filename you want to write it to.\n', metavar='-f')
 
 parser.add_argument('-mqtt', action='store_true',
-    help='Filename you want to write it to.\n', metavar='-mqtt')
+    help='If you want to send it over mqtt (config needed).\n', metavar='-mqtt')
 
 args = parser.parse_args()
 
